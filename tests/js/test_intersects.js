b2284b8b41b5eb03fff294331d6b204a9a0654c2
codyThompson <cody.thompson22@gmail.com>
2016-08-25 23:24:36 -0700

    finished off housekeeping for 0.0.0

--- tests/js/test_intersects.js
+++ tests/js/test_intersects.js
@@ -11,13 +11,6 @@ test_suite = {
    * TODO: write actual tests, and then implement
    */
   tests: {
-    "polypick.intersects": {
-      ray_tri: function (test_vars) {
-        chai.expect(polypick.intersects.ray_tri)
-          .to.throw('not implemented');
-      }, // end ray_tri category
-    }, // end polypick.intersects category
-
     polypick: {
       version: function (test_vars) {
         var expected_ver = '0.0.0';
@@ -29,6 +22,30 @@ test_suite = {
         chai.expect(polypick.pick)
           .to.throw('not implemented');
       } // end pick category
-    } // end polypick category
+    }, // end polypick category
+
+    "polypick.intersects": {
+      ray_tri: function (test_vars) {
+        chai.expect(polypick.intersects.ray_tri)
+          .to.throw('not implemented');
+      }, // end ray_tri category
+    }, // end polypick.intersects category
+
+    // these tests act a checklist for release stuff
+    "housekeeping": {
+      // the version test above should be updated as the first commit to of a
+      // new version
+      "update version number test": function (test_vars) {
+        var check = true;
+        chai.expect(check).to.equal(true, 'this should happen in the first commit of a new version');
+      },
+
+      // this should be changed in the commit before release
+      "update roadmap and/or README.md": function (test_vars) {
+        var check = true;
+        chai.expect(check).to.equal(true, 'this should happen in the last commit of a new version');
+      },
+    }
+
   },
 };
